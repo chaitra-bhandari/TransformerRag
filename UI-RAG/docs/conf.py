c78eed374f91ae1d08e73d3a4cef84296c00c937
Bhandari <chaitra.bhandari@siemens-energy.com>
2026-03-10 19:40:51 +0100

    added files

---
+++ UI-RAG/docs/conf.py
@@ -0,0 +1,12 @@
+project = 'Transformer Spec RAG'
+copyright = '2024, Your Name'
+author = 'Your Name'
+release = '1.0.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+html_theme = 'sphinx_rtd_theme'
+master_doc = 'index'
---
+++ UI-RAG/docs/index.rst
@@ -0,0 +1,15 @@
+Transformer Spec RAG
+====================
+
+Welcome to the documentation.
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   installation
+   configuration
+   usage
+```
+
+---
